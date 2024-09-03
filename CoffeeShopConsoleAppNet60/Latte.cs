f1009a393ddb738c0b1a0d6000255c9b538136a0
abdiabbaz <abdiabbas@outlook.dk>
2024-09-03 13:14:56 +0200

    Worked till task 12

---
+++ CoffeeShopConsoleAppNet60/Latte.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShopConsoleAppNet60
+{
+    public class Latte : Coffee, IMilk
+    {
+        public Latte():this(0,"")
+        {
+        }
+
+        public Latte(int discount, string blend) : base(discount, blend)
+        {
+        }
+
+        public int mlMilk()
+        {
+            return 200;
+        }
+
+        public override int Price()
+        {
+            if (Discount > 5)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            return 40 - Discount;
+        }
+
+        public override string Strength()
+        {
+            return "Medium";
+        }
+    }
+}
--- CoffeeShopConsoleAppNet60/Program.cs
+++ CoffeeShopConsoleAppNet60/Program.cs
@@ -1,2 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using CoffeeShopConsoleAppNet60;
+
 Console.WriteLine("Hello, World!");
+
+
+Console.WriteLine("================> Printing every coffee <================");
+BlackCoffee BlackCoffee = new BlackCoffee();
+Latte Latte = new Latte(3,"");
+Cortado Cortado = new Cortado();
+
+List<Coffee> coffees = new List<Coffee>();
+coffees.Add(BlackCoffee);
+coffees.Add(Latte);
+coffees.Add(Cortado);
+
+List<IMilk> OnlyMilks = new List<IMilk>();
+IMilk WithMilk = new Latte();
+IMilk WithMilk1 = new Cortado();
+
+OnlyMilks.Add(WithMilk);
+OnlyMilks.Add(WithMilk1);
+
+foreach (var coffee in coffees)
+{
+    Console.WriteLine($"The price of coffee is {coffee.Price()} and the strength is {coffee.Strength()}");
+
+}
+
+
